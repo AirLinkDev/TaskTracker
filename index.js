317f36875c781371b70a066a4b9326a2cdce68be
AirLinkDev <thomas.trebacz@gmail.com>
2023-10-09 09:12:24 -0700

    My EJS template is rendered on the server, not in the browser, so I don't have access to the DOM in my EJS templates

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
+app.use(bodyParser.urlencoded({ extended: true }));
+var tasks = new Array();
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
 */
 app.get("/", (req, res) => {
+
   res.render("index.ejs");
 });
 /*
@@ -21,11 +24,31 @@ app.get("/workTask", (req, res) => {
 });
 
 /*
-  Hint: Check the nav bar in the header.ejs to see the button hrefs
-Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. 
-
-ans: I added <%- include(“header.ejs”)%> to the ejs of about and contact
+The list of tasks created by the user has session lifespan
+I've elected to maintain the array of task objects at the server
+because i'm not sure if the locals.tasks would not be reset with every page reload
 */
+app.post("/submit", (req, res) => {
+  /* 
+  Assemble incoming variables into task object
+  */
+ console.log("Received new task: ",req.body.Task);
+ console.log("Received new deadline: ",req.body.DueDate);
+  const task = {
+    
+    newTask : req.body.Task,
+    dueDate : req.body.DueDate,
+    isDone  : false
+  };
+  tasks.push(task);
+  const data = {
+    Tasks : tasks
+  }
+/*
+Send all tasks up to the client 
+*/
+    res.render("index.ejs", data);  
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
